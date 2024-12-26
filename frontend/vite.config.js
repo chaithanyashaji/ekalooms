a397d27c0ede421a766c66fba46b49e37bfa6ffb
Chaithanya Shaji <139004346+chaithanyashaji@users.noreply.github.com>
2024-12-26 18:49:50 +0530

    site map,logo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{port: 5174}
-})
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://ekalooms.com', // Your domain name
+      // Optional configurations:
+      // Exclude specific pages from the sitemap
+      // exclude: ['/exclude-page'],
+      // Specify routes manually if needed
+      // routes: ['/about', '/contact'],
+    }),
+  ],
+  server: {
+    port: 5174, // Your development server port
+  },
+});
