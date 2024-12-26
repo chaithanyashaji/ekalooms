fe65ad096d5c40ee6bf627b297fb25537fec5f5d
Chaithanya Shaji <139004346+chaithanyashaji@users.noreply.github.com>
2024-12-26 21:06:57 +0530

    photos

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,19 +1,7 @@
 
 
 import logo from './logo.png'
-import hero_img1 from './hero_img1.jpg'
-import hero_img2 from './hero_img2.jpg'
-import hero_img3 from './hero_img3.jpg'
-import hero_img4 from './hero_img4.jpg'
-import hero_img41 from './hero_img41.jpg'
-import hero_img5 from './hero_img5.jpg'
-import hero_img6 from './hero_img6.jpg'
-import hero_img7 from './hero_img7.jpg'
-import hero_img8 from './hero_img8.jpg'
-import hero_img9 from './hero_img9.jpg'
-import hero_img10 from './hero_img10.jpg'
 
-import hero_img11 from './hero_img11.jpg'
 
 import bin_icon from './bin_icon.png'
 import dropdown_icon from './dropdown_icon.png'
@@ -25,19 +13,8 @@ import dropdown_icon from './dropdown_icon.png'
 
 
 export const assets = {
-    logo,
-    hero_img1,
-    hero_img2,
-    hero_img3,
-    hero_img4,
-    hero_img41,
-    hero_img5,
-    hero_img6,
-    hero_img7,
-    hero_img8,
-    hero_img9,
-    hero_img10,
-    hero_img11,
+    
+   
    
    
     dropdown_icon,
